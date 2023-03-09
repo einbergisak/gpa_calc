9f896a77e5f613f592a4948f40d041740223a747
Isak Einberg <einbergisak@gmail.com>
2023-03-09 17:00:24 +0100

    Add CDATE20 course list

---
+++ CDATE20_courses.py
@@ -0,0 +1,22 @@
+# Mandatory (A-F graded) courses for the first three years of the CDATE program at KTH, for students admitted in 2020.
+CDATE20_COURSES = [
+    ("iskriv", 4.5, 'F'),
+    ("basmatte", 7.5, 'F'),
+    ("algdat", 6.0, 'F'),
+    ("prog", 7.0, 'F'),
+    ("envarre", 7.5, 'F'),
+    ("linalg", 7.5, 'F'),
+    ("MDI", 6.0, 'F'),
+    ("nummen", 6.0, 'F'),
+    ("orkan", 6.0, 'F'),
+    ("dbas", 6.0, 'F'),
+    ("dtek", 9.0, 'F'),
+    ("logik", 7.5, 'F'),
+    ("flervarre", 7.5, 'F'),
+    ("progp", 6.0, 'F'),
+    ("sanstat", 6.0, 'F'),
+    ("diskmatte", 6.0, 'F'),
+    ("ADK", 9.5, 'F'),
+    ("OS", 6.0, 'F'),
+    ("hållut", 7.5, 'F')
+]
